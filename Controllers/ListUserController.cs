23ee8f33e8afea1e97368678c573381978e63493
dovgalev92 <dovgalev2021@inbox.ru>
2022-11-08 01:57:14 +0300

    Добавил класс CreateModel. Создал контроллер ListUser и настроил в action поиск гражданина

---
+++ Controllers/ListUserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project_MVC.Context_DataBase;
+using Project_MVC.Models;
+
+namespace Project_MVC.Controllers
+{
+    public class ListUserController : Controller
+    {
+        private readonly ContextDb context;
+        public CreateModel model;
+
+
+        public ListUserController(ContextDb _context, CreateModel model)
+        {
+            context = _context;
+            this.model = model;
+        }
+
+        public async Task<IActionResult> ListUser(string search)
+        {
+            var result_search = from user in context.Users select user;
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                result_search = context.Users.Where(u => u.Name.Contains(search) || u.Surname.Contains(search) || u.Patronymic.Contains(search))
+                    .Include(c => c.Category).Include(l => l.Locality).Include(s => s.Street);
+                return View(await result_search.ToListAsync());
+            }
+            return View(await context.Users.Include(c => c.Category).Include(l => l.Locality).Include(s => s.Street).ToListAsync());
+        }
+    }
+}
---
+++ Models/CreateModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Project_MVC.Entity;
+
+namespace Project_MVC.Models
+{
+    public class CreateModel
+    {
+        public User? User { get; set; }
+        public IEnumerable<SelectListItem>? Category { get; set; }
+        public IEnumerable<SelectListItem>? Locality { get; set; }
+        public IEnumerable<SelectListItem>? Street { get; set; }
+    }
+}
