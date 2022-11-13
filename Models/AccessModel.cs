79a5fb874f6d5966113b63549b521e7212885a19
dovgalev92 <dovgalev2021@inbox.ru>
2022-11-14 00:12:29 +0300

    Добавил класс AccessModel для доступа к полям классов при регистрации и входе в приложение. Добавил соответсвуещее View()

---
+++ Models/AccessModel.cs
@@ -0,0 +1,10 @@
+﻿using Project_MVC.Entity.AccessUsers;
+
+namespace Project_MVC.Models
+{
+    public class AccessModel
+    {
+        public LogInUser? LogInUser { get; set; }
+        public RegistUser? RegistUser { get; set; }
+    }
+}
